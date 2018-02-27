64c608a09f161c3b23112b6c03286103bf9d311d
Sébastien Barre <SBarre.prestataire@voyages-sncf.com>
2018-02-27 14:32:38 +0100

    WIP

--- newReleaseProcess.groovy
+++ newReleaseProcess.groovy
@@ -76,7 +76,7 @@ pipeline {
 			steps {
 				script{
 					gitUtils.mergeAllProjects(group: GROUP, repositories: ALL_REPOS, branchFrom: "develop", branchTo: "master")
-					//gitUtils.pushAllModifications(group: GROUP, repositories: ALL_REPOS, branch: "master")
+					gitUtils.pushAllModifications(group: GROUP, repositories: ALL_REPOS, branch: "master")
 				}
 			}
 		}
@@ -97,7 +97,7 @@ pipeline {
 			steps {
 				script{
 					gitUtils.mergeAllProjects(group: GROUP, repositories: ALL_REPOS, branchFrom: "master", branchTo: "develop")
-					//gitUtils.pushAllModifications(group: GROUP, repositories: ALL_REPOS, branch: "develop")
+					gitUtils.pushAllModifications(group: GROUP, repositories: ALL_REPOS, branch: "develop")
 				}
 			}
 		}

3a95f304ef22a5359594a15721302b022c9222e7
Sébastien Barre <SBarre.prestataire@voyages-sncf.com>
2018-02-28 10:24:43 +0100

    WIP

--- newReleaseProcess.groovy
+++ newReleaseProcess.groovy
@@ -100,7 +100,6 @@ pipeline {
 			steps {
 				script {
 					if (RELEASE_KAFKA_SER == true) {
-						echo "release kafka"
 						pomUtils.setArtifactVersionInDependencyManagement(group: GROUP, repository:"houston-parent", artifactId:"cosmo-kafka-serialization", version: KFK_RELEASE_VERSION, isDryRun: params.IS_DRY_RUN)
 					}
 					pomUtils.setArtifactVersionInDependencyManagement(group: GROUP, repository:"houston-parent", artifactId:"houston-common", version: HOUSTON_RELEASE_VERSION, isDryRun: params.IS_DRY_RUN)
@@ -133,7 +132,7 @@ pipeline {
 		stage("Release component") {
 			steps {
 				script {
-					releaseUtils.releaseThisProject(group: GROUP, repositories:"houston-connector-emeraude", nextVersion: params.HOUSTON_NEXT_DEV_VERSION, isDryRun: params.IS_DRY_RUN)
+					releaseUtils.releaseThisProject(group: GROUP, repository:"houston-connector-emeraude", nextVersion: params.HOUSTON_NEXT_DEV_VERSION, isDryRun: params.IS_DRY_RUN)
 				}
 			}
 		}
